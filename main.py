4beb0a32b082b87159d581601a26963bedc0ed4e
Paul Bonduelle <paul@feedly.com>
2020-04-14 10:26:21 +0200

    wip

--- main.py
+++ main.py
@@ -167,6 +167,10 @@ for epoch in range(1, 10):
         # Label based metrics
         target2.cpu()
         predictions.cpu()
+        print(target2.device)
+        print(predictions.device)
+        print(data.device)
+        aa = ((predictions == 1) & (target2 == 1))
         label_based_TP += ((predictions == 1) & (target2 == 1)).sum(dim=0)
         label_based_TN += ((predictions == 0) & (target2 == 0)).sum(dim=0)
         label_based_FP += ((predictions == 1) & (target2 == 0)).sum(dim=0)
